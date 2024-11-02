fc7d9b0b3b0a7c2923daa47bba49b280481ff45f
Duc Le <minhduc.le@mail.utoronto.ca>
2024-11-02 01:41:35 -0400

    Labmachine doesnt have python3-env

--- lua/custom/plugins/lsp/init.lua
+++ lua/custom/plugins/lsp/init.lua
@@ -79,18 +79,10 @@ return { -- LSP Configuration & Plugins
     -- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     local servers = {
+      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       clangd = {
         filetypes = { 'c', 'cpp', 'h', 'hpp' },
       },
-      cmake = {},
-      -- gopls = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
       pyright = {},
 
       bashls = {},
